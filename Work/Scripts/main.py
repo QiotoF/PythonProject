9b77bdc4137cf1e8cbd57e1dfc8d3fe903fea86c
Dmitry <kyloren228@gmail.com>
2019-05-19 01:52:16 +0300

    Add function of adding a videocard to db1

--- Work/Scripts/main.py
+++ Work/Scripts/main.py
@@ -18,26 +18,55 @@ from tkinter import IntVar
 # import databin
 
 class MainWindow:
-    def __init__(self, master, df):
+    def __init__(self, master):
+
+        self.OPTIONS = [0, 1, 2, 3, 4, 5]
+
+        self.df1 = pd.read_csv('../Data/bd1.csv')
+        self.df1.index = ([(x, y) for x, y in zip(self.df1['Название'], self.df1['Конфигурация памяти'])])
+        self.df2 = pd.read_csv('../Data/bd2.csv')
+        self.df2.index = (x for x in self.df2['Название'])
+        self.df3 = pd.read_csv('../Data/bd3.csv')
+        self.df3.index = (x for x in self.df3['Название'])
+        self.df4 = pd.read_csv('../Data/bd4.csv')
+        self.df4.index = (x for x in self.df4['Название'])
+        self.df5 = pd.read_csv('../Data/bd5.csv')
+        self.df5.index = (x for x in self.df5['Название'])
+
+        self.data_frames = [self.df1, self.df2, self.df3, self.df4, self.df5]
+
+        self.df = self.df1
+
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.df = df
         self.btn_new = tk.Button(self.frame, text='New', command=self.open_new_window)
         self.btn_new.pack()
-        self.tree = ttk.Treeview(self.frame, columns=tuple(self.df.columns), show='headings')
 
         self.selected_database = IntVar(self.frame)
-        self.selected_database.set(OPTIONS[0])
-        self.options_menu = ttk.OptionMenu(self.frame, self.selected_database, *OPTIONS)
+        self.selected_database.set(self.OPTIONS[1])
+        self.options_menu = ttk.OptionMenu(self.frame, self.selected_database, *self.OPTIONS)
         self.options_menu.pack()
         self.selected_database.trace('w', self.change_database)
 
-        for x in df.columns:
-            self.tree.heading(x, text=x)
-        for index, row in df.iterrows():
-            self.tree.insert("", "end", index, values=list(row))
+        self.tree = self.new_tree(self.df)
+
         self.tree.pack()
         self.frame.pack()
+
+    def insert_new_entry(self, entry):
+        self.df1.index = pd.MultiIndex.from_tuples(self.df1.index)
+        self.df1.loc[
+            (entry['Название'], entry['Конфигурация памяти']), ['Название', 'Дата выхода', 'Конфигурация памяти',
+                                                                'Энергопотребление, Вт',
+                                                                'Far Cry 5, FPS',
+                                                                'Fallout 4, FPS', 'The Witcher 3, FPS',
+                                                                '3DMark Cloud Gate', '3DMark Fire Strike',
+                                                                'Средняя цена, ₽']] = (
+            entry['Название'], entry['Дата выхода'], entry['Конфигурация памяти'], entry['Энергопотребление, Вт'],
+            entry['Far Cry 5, FPS'], entry['Fallout 4, FPS'],
+            entry['The Witcher 3, FPS'], entry['3DMark Cloud Gate'],
+            entry['3DMark Fire Strike'], entry['Средняя цена, ₽'])
+        self.update_table()
 
     def open_new_window(self):
         self.window_new = tk.Toplevel(self.master)
@@ -45,7 +74,7 @@ class MainWindow:
 
     def change_database(self, *args):
         self.tree.destroy()
-        self.df = dataframes[self.selected_database.get() - 1]
+        self.df = self.data_frames[self.selected_database.get() - 1]
         self.tree = self.new_tree(self.df)
         self.tree.pack()
 
@@ -56,7 +85,7 @@ class MainWindow:
         for index, row in df.iterrows():
             tree.insert("", "end", index, values=list(row))
         return tree
-    
+
     def update_table(self):
         self.tree.delete(*self.tree.get_children())
         for x in self.df.columns:
@@ -70,7 +99,7 @@ class NewWindow:
         self.master = master
         self.frame = tk.Frame(self.master)
         self.main_window = main_window
-        
+
         tk.Label(self.frame, text='Новая запись в базе данных').grid(row=0, columnspan=2)
         tk.Label(self.frame, text='Имя:').grid(row=1, column=0)
         tk.Label(self.frame, text='Дата выхода:').grid(row=2, column=0)
@@ -82,11 +111,11 @@ class NewWindow:
         tk.Label(self.frame, text='3DMark Cloud Gate').grid(row=8, column=0)
         tk.Label(self.frame, text='3DMark Fire Strike').grid(row=9, column=0)
         tk.Label(self.frame, text='Средняя цена, руб.').grid(row=10, column=0)
-        
+
         self.entry_name = tk.Entry(self.frame)
-        self.entry_name.grid(row=1, column=1)        
+        self.entry_name.grid(row=1, column=1)
         self.entry_date = tk.Entry(self.frame)
-        self.entry_date.grid(row=2, column=1)        
+        self.entry_date.grid(row=2, column=1)
         self.entry_memory = tk.Entry(self.frame)
         self.entry_memory.grid(row=3, column=1)
         self.entry_power = tk.Entry(self.frame)
@@ -103,44 +132,39 @@ class NewWindow:
         self.entry_firestrike.grid(row=9, column=1)
         self.entry_price = tk.Entry(self.frame)
         self.entry_price.grid(row=10, column=1)
-        
+
         tk.Button(self.frame, text='Добавить', command=self.new_entry).grid(row=11, columnspan=2)
         self.frame.pack()
 
     def new_entry(self):
-        key = (self.entry_name.get(), self.entry_memory.get())
-        value = (
-                self.entry_name.get(),
-                self.entry_date.get(),
-                self.entry_memory.get(),
-                self.entry_power.get(),
-                self.entry_farcry5.get(),
-                self.entry_fallout4.get(),
-                self.entry_thewitcher3.get(),
-                self.entry_cloudgate.get(),
-                self.entry_firestrike.get(),
-                self.entry_price.get()
-                )
-        self.main_window.df.index = pd.MultiIndex.from_tuples(self.main_window.df.index)
-        self.main_window.df.loc[('df', 'fd'), ['Название', 'Дата выхода', 'Конфигурация памяти', 'Энергопотребление, Вт', 'Far Cry 5, FPS', 'Fallout 4, FPS', 'The Witcher 3, FPS', '3DMark Cloud Gate', '3DMark Fire Strike', 'Средняя цена, ₽']] = ('f', 'f', 'f', 0, 0, 0, 0, 0, 1, 0)
-        self.main_window.update_table()
+        keys = ['Название', 'Дата выхода', 'Конфигурация памяти', 'Энергопотребление, Вт', 'Far Cry 5, FPS',
+                'Fallout 4, FPS', 'The Witcher 3, FPS', '3DMark Cloud Gate', '3DMark Fire Strike',
+                'Средняя цена, ₽']
+        values = [
+            self.entry_name.get(),
+            self.entry_date.get(),
+            self.entry_memory.get(),
+            self.entry_power.get(),
+            self.entry_farcry5.get(),
+            self.entry_fallout4.get(),
+            self.entry_thewitcher3.get(),
+            self.entry_cloudgate.get(),
+            self.entry_firestrike.get(),
+            self.entry_price.get()
+        ]
+        entry = dict(zip([x for x in keys], [y for y in values]))
+        self.main_window.insert_new_entry(entry)
+        self.master.destroy()
+        # self.main_window.df.index = pd.MultiIndex.from_tuples(self.main_window.df.index)
+        # self.main_window.df.loc[
+        #     (values[0], values[2]), ['Название', 'Дата выхода', 'Конфигурация памяти', 'Энергопотребление, Вт', 'Far Cry 5, FPS',
+        #                    'Fallout 4, FPS', 'The Witcher 3, FPS', '3DMark Cloud Gate', '3DMark Fire Strike',
+        #                    'Средняя цена, ₽']] = (values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7],
+        #                                           values[8], values[9])
+        # self.main_window.update_table()
 
 
 # dataframe = databin.read_from_binary('../Data/data')
-df1 = pd.read_csv('../Data/bd1.csv')
-df1.index = ([(x, y) for x, y in zip(df1['Название'], df1['Конфигурация памяти'])])
-df2 = pd.read_csv('../Data/bd2.csv')
-df2.index = (x for x in df2['Название'])
-df3 = pd.read_csv('../Data/bd3.csv')
-df3.index = (x for x in df3['Название'])
-df4 = pd.read_csv('../Data/bd4.csv')
-df4.index = (x for x in df4['Название'])
-df5 = pd.read_csv('../Data/bd5.csv')
-df5.index = (x for x in df5['Название'])
-
-OPTIONS = [1, 2, 3, 4, 5]
-
-dataframes = [df1, df2, df3, df4, df5]
 
 
 # def change_database(*args):
@@ -149,8 +173,8 @@ dataframes = [df1, df2, df3, df4, df5]
 #    tree = new_tree(dataframes[selected_database.get() - 1])
 #    tree.pack()
 
-def new_entry(name):
-    print(name)
+# def new_entry(name):
+#     print(name)
 
 
 # window_new = tk.Tk()
@@ -163,8 +187,8 @@ def new_entry(name):
 # entry_memory = tk.Entry(window_new).grid(row=3, column=1)
 # tk.Button(window_new, text='Добавить', command=new_entry).grid(row=4, columnspan=2)
 
-def open_new_window():
-    window_new.mainloop()
+# def open_new_window():
+#     window_new.mainloop()
 
 
 # window = tk.Tk()
@@ -184,21 +208,21 @@ def open_new_window():
 #    tree.insert("", "end", index, values=list(row))
 # tree.pack()
 
-
-def new_tree(df):
-    tree = ttk.Treeview(window, columns=tuple(df.columns), show='headings')
-    for x in df.columns:
-        tree.heading(x, text=x)
-    for index, row in df.iterrows():
-        tree.insert("", "end", index, values=list(row))
-    return tree
+#
+# def new_tree(df):
+#     tree = ttk.Treeview(window, columns=tuple(df.columns), show='headings')
+#     for x in df.columns:
+#         tree.heading(x, text=x)
+#     for index, row in df.iterrows():
+#         tree.insert("", "end", index, values=list(row))
+#     return tree
 
 
 # window.mainloop()
 
 def main():
     root = tk.Tk()
-    app = MainWindow(root, df1)
+    app = MainWindow(root)
     root.mainloop()
 
 
